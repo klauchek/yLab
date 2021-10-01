8dc0f2fe06be7cb8c04d604913210906ac1ad3e1
klauchek <kniazeva.ms@phystech.edu>
2021-10-01 06:37:52 +0300

    finished

---
+++ Cache/README.md
@@ -0,0 +1,39 @@
+LFU Cache
+========================
+### First ILab course project
+# Building a  project
+```
+  $ mkdir build
+  $ cd build
+  $ cmake ..
+  $ make
+```
+# Running tests
+To run tests, cd to the project directory `Cache`, then to `build` and run
+```
+/LFU_cache < ../tests/test1.txt
+```
+Do the same for other tests.
+
+
+# Algorithm
+
+A cache eviction algorithm includes 3 different operations on the caches data.
+- Set (or insert) an item in the cache.
+- Retrieve (or lookup) an item in the cache, incrementing its usage count.
+- Evict (or delete) rhe Least Frequently Used item from the cache.
+
+We use:
+- a hastable to access elements by key
+- a doubly linked list to link together nodes with the same access frequency
+- a doubly linked list as the frequency 
+
+The algorithm is shown in the pictures below.
+ ![Screenshot](pics/1.png)
+
+
+ ![Screenshot](pics/2.png)
+================
+
+
+### main resource: http://dhruvbird.com/lfu.pdf
--- Cache/lfu.h
+++ Cache/lfu.h
@@ -9,44 +9,50 @@
 #include <vector>
 #include <cassert>
 
+
+//! Declaration of Item structure
 template <typename T>
 struct Item;
 
+//! Declaration of FreqNode structure
 template <typename T>
 struct FreqNode;
 
 
-
+//! typedef for iterator of std::list<FreqNode>
 template <typename T>
 using FreqIt = typename std::list<FreqNode<T>>::iterator;
 
-
+//! typedef for iterator of std::list<Item>
 template <typename T>
 using ItemIt = typename std::list<Item<T>>::iterator;
 
 
+//! Definition of FreqNode structure
 template <typename T>
 struct FreqNode
 {
     int hits_;
     std::list<Item<T>> items_;
 
-    //constructor
+    //!Ctor
     FreqNode(int hits = 1) : hits_(hits), items_() {}
 };
 
 
+//! Definition of Item structure
 template <typename T>
 struct Item
 {
     T data_;
     FreqIt<T> parent_;
     
-    //constructor
+    //!Ctor
     Item(T data, FreqIt<T> parent) : data_(data), parent_(parent) {}
 };
 
 
+//! Class for LFU algorithm
 template <typename T>
 class Cache
 {
@@ -54,18 +60,26 @@ private:
 
     size_t capacity = 0;
     
+    //! List of FreqNodes
     std::list<FreqNode<T>> freqlist_;
 
+    //! typedef for iterator of std::unordered_map<T, ItemIt<T>>
     std::unordered_map<T, ItemIt<T>> hash_;
     using HashIt = typename std::unordered_map<T, ItemIt<T>>::iterator;
     
 
 public:
 
-    //constructor
+    //!Ctor
     Cache(size_t cap) : capacity(cap) {}
 
-
+    //--------------------------------------------------------
+    //! Function for checking if there is an item in the cache
+    //! @param[in] elem   requested element
+    //!
+    //! @return true if cache hit happened
+    //!         false if element doesn't exist in the cache
+    //--------------------------------------------------------
 
     bool lookup(const T elem)
     {
@@ -75,7 +89,7 @@ public:
         //if there's no such element in the map
         if (entry == hash_.end())
         {
-            
+
             //element wasn't found, insert new
             insert_item(elem);
             
@@ -87,6 +101,12 @@ public:
         return true;
     }
 
+    //--------------------------------------------------------
+    //! Function for testing LFU algorithm
+    //! @param[in] calls   number of requests
+    //!
+    //! @return number of cache hits
+    //--------------------------------------------------------
 
     int test_processing(int calls)
     {
@@ -109,38 +129,51 @@ public:
 
 private:
 
-    //delete least frequetly used element
+
+    //! Function for deleting least frequently used element from the cache
+
     void delete_lf_used()
     {
         if (freqlist_.size() > 0)
         {
-        hash_.erase(freqlist_.front().items_.back().data_);//delete el from hashtable
-        freqlist_.front().items_.pop_back();//delete from freqlist
+            //delete element from hashtable and from freqlist
+            hash_.erase(freqlist_.front().items_.back().data_);
+            freqlist_.front().items_.pop_back();
 
-        //if items_ become empty after deletion
-        if (freqlist_.front().items_.empty())
-            freqlist_.pop_front();//delete freqnode from freqlist
+            //if items_ become empty after deletion -> delete freqnode from freqlist
+            if (freqlist_.front().items_.empty())
+                freqlist_.pop_front();
         }
     }
 
-
+    //--------------------------------------------------------
+    //! Function for inserting an element into the cache
+    //! @param[in] elem   element to insert
+    //--------------------------------------------------------
 
     void insert_item (const T elem)
     {
         if (hash_.size() >= capacity)
             delete_lf_used();
-        //добавили еще один частотный узел в начало фреклиста, если недостает
+
+        //adding ine more freqnode if necessary
         if (freqlist_.empty() || freqlist_.front().hits_ != 1)
             freqlist_.push_front(FreqNode<T>());
         
+        //adding the element to the freqlist and to the hashtable
         freqlist_.front().items_.push_front((Item<T>(elem, freqlist_.begin())));
         hash_.insert({elem, freqlist_.front().items_.begin()});
 
     }
 
+    //--------------------------------------------------------
+    //! Function for incrementing usage frequency of the element
+    //! @param[in] item_it   iterator to the element that was inserted
+    //--------------------------------------------------------
+
     void incr_frequency(ItemIt<T> item_it)
     {
-
+        //if there was only one freqnode in the freqlist before inserting new element
         if (next(item_it->parent_) == freqlist_.end())
         {
             freqlist_.push_back(FreqNode<T>(item_it->parent_->hits_ + 1));
@@ -150,7 +183,7 @@ private:
         FreqIt<T> next_freq_it = ++freq_it;
         --freq_it;
 
-        //element with freq + 1 already exists
+        //if at least one element with incremented frequency already exists
         if (next_freq_it->hits_ == (freq_it->hits_ + 1))
         {
             next_freq_it->items_.push_front(Item<T> (item_it->data_, next_freq_it));
@@ -158,7 +191,7 @@ private:
         }
         else
         {
-            //element with freq doesnt exist
+            //if element with incremented frequency doesn't exist
             FreqIt<T> new_freq_it = freqlist_.insert(next_freq_it, FreqNode<T>(freq_it->hits_ + 1));
             new_freq_it->items_.push_front(Item<T>(item_it->data_, new_freq_it));
             Change_freq(item_it, freq_it, new_freq_it);
@@ -166,10 +199,17 @@ private:
     }
 
 
+    //--------------------------------------------------------
+    //! Function for retying the element to the new freqnode
+    //! @param[in] item_it   iterator to the element that will be retied
+    //! @param[in] cur       iterator to the current freqnode
+    //! @param[in] fut       iterator to the next freqnode (with incremented frequency)
+    //--------------------------------------------------------
     void Change_freq(ItemIt<T> item_it, FreqIt<T> cur, FreqIt<T> fut)
     {
         hash_.erase(item_it->data_);
         hash_.insert({item_it->data_, fut->items_.begin()});
+        cur->items_.erase(item_it);
         if(cur->items_.empty())
             freqlist_.erase(cur);
     }
