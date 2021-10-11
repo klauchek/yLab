56661764181583b61df7620cd150d37dd3bc5ab2
klauchek <kniazeva.ms@phystech.edu>
2021-10-11 17:21:32 +0300

    deleted unnecessary dirs

--- Cache/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mash/Code/ILab2/Cache
-# Build directory: /home/mash/Code/ILab2/Cache/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
